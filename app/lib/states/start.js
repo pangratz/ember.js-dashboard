a3c807f6a93934245f450feaa08ae295eb07b293
pangratz <cmueller.418@gmail.com>
2012-04-08 00:17:43 +0200

    remove - for now - unused stuff like routing and store

--- app/lib/states/start.js
+++
@@ -1,9 +0,0 @@
-require('dashboard/core');
-
-App.StartState = Ember.ViewState.extend({
-
-  view: Ember.View.extend({
-    templateName: 'ember-skeleton/~templates/main_page'
-  })
-
-});
--- app/lib/store.js
+++
@@ -1,3 +0,0 @@
-require('dashboard/core');
-
-App.store = DS.Store.create();
